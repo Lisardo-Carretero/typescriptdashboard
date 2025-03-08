e601847913036b054b0aaaf0e8cf2a6982c03f49
Lisardo Carreter Colmenar <Lisardokill@hotmail.com>
2025-03-08 18:15:03 +0100

    fix; add databasetype

--- app/api/alerts/route.ts
+++ app/api/alerts/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from "../../database.types";
 
-const supabase = createClient(
+
+const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
